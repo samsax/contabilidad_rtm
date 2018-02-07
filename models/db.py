634182949f03833167ec335c821e18afa578f0d1
samsax <samuelsaxofon@gmail.com>
2018-02-07 08:28:47 -0600

    bases de datos pruebas y pro

--- models/db.py
+++ models/db.py
@@ -32,7 +32,7 @@ if not request.env.web2py_runtime_gae:
     # ---------------------------------------------------------------------
     db = DAL(
             #'postgres://Postgres:TbwKR-8u@localhost/contabilidad',
-            'mysql://samsax:contabilidad@samsax.mysql.pythonanywhere-services.com/contabilidad_rtm',
+            'postgres://fcwlgkzj:pY-Q7leW8agrZIFQLIcEsui3rrVTBVLQ@elmer.db.elephantsql.com:5432/fcwlgkzj',
             migrate=True,
             check_reserved=['all']
             )
